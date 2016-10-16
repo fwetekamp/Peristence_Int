ff87814f40f3cc0463f6705d3b33e695065002cf
fwetekamp <fabian.we@gmail.com>
2016-10-16 12:55:30 +0200

    Sample Project that shows simple data persistence
    Sample Project that shows simple data persistence for iOS10, written in
    Swift3

--- Peristence_Int/ViewController.swift
+++ Peristence_Int/ViewController.swift
@@ -1,25 +1,62 @@
 //
 //  ViewController.swift
-//  Peristence_Int
+//  testingpersistence2
 //
-//  Created by Fabian Wetekamp on 16/10/2016.
+//  Created by Fabian Wetekamp on 15/10/2016.
 //  Copyright © 2016 Fabian Wetekamp. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var users = [Points]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let savedBalance = loadBalance(){
+            users += savedBalance
+        }else{
+            loadSampleUsers()
+        }
+        let lastbalancestring:Int = (users.last?.balance)!
+        lastsaveduser.text = String(lastbalancestring)
     }
-
+    
+    func  loadSampleUsers(){
+        let user1 = Points(balance: 5)
+        users = [user1!]
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBOutlet weak var lastsaveduser: UILabel!
+    @IBOutlet weak var enternewuser: UITextField!
+    
+    @IBAction func updatebalance(_ sender: AnyObject) {
+        let newvalue = enternewuser.text
+        lastsaveduser.text = newvalue
+        let newvalueint:Int = Int(newvalue!)!
+        users.append(Points(balance: newvalueint)!)
+        saveBalance()
+    }
+    
+    
+    func saveBalance(){
+        let isSuccessfullSave = NSKeyedArchiver.archiveRootObject(users, toFile: Points.ArchiveURL.path)
+        if !isSuccessfullSave{
+            print("Failed to save balance")
+        }
+    }
+    
+    func  loadBalance() -> [Points]?{
+        return NSKeyedUnarchiver.unarchiveObject(withFile: Points.ArchiveURL.path) as? [Points]
+    }
+    
+    
 }
 
